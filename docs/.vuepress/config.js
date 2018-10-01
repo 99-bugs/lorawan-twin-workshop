c509fae78b1eadb17f5c9cbcbb1ddfe3b529a9a0
Nico De Witte <nico.dewitte@vives.be>
2018-10-01 16:59:50 +0200

    Remove internal links from top nav

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,10 +3,7 @@ module.exports = {
   description: 'Introductie tot LoRaWAN aan de hand van een STEM workshop',
   themeConfig: {
     nav: [
-      {text: 'LoRaWAN', link: '/lorawan/'},
-      {text: 'Sodaq Explorer', link: '/sodaq-explorer/'},
-      {text: 'Sensors', link: '/sensors/'},
-      {text: 'VIVES', link: 'https://www.vives.be'},
+      {text: 'VIVES', link: 'https://www.vives.be'}
     ],
     displayAllHeaders: true,
     sidebar: 'auto',
