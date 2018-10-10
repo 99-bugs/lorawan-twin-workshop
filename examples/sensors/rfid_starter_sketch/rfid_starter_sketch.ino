1c49d8591409efb75175ff7c1a0f7a69f4f2fda3
Nico De Witte <nico.dewitte@vives.be>
2018-10-10 11:47:11 +0200

    Refactor RFID to match the lorawan starter sketch

--- examples/sensors/rfid_starter_sketch/rfid_starter_sketch.ino
+++ examples/sensors/rfid_starter_sketch/rfid_starter_sketch.ino
@@ -1,44 +1,41 @@
+const int SIZE_OF_BUFFER = 32;
+uint8_t buffer[SIZE_OF_BUFFER];
+
 void setup() {
+    // put your setup code here, to run once:
+    SerialUSB.begin(115200);
+    while ((!SerialUSB) && (millis() < 5000));
+    SerialUSB.println("Starten van RFID demo");
     Serial.begin(9600);         // Serial is verbonden met de RFID lezer
-    SerialUSB.begin(115200);    // De terminal voor de user
 }
  
 void loop()
 {
-    // Kijk of er een kaart is en zoja lees het ID
-    String id = try_to_read_card();
-    if (id.length() > 0) {
-      SerialUSB.print("Kaart gedetecteerd met ID = ");
-      SerialUSB.println(id);
-    }
+    // Lees het id van een RFID kaart
+    read_rfid_card();   // Blocking
+    
+    SerialUSB.print("Kaart gedetecteerd met ID = ");
+    SerialUSB.println((char*)buffer);
 }
 
-void clear_buffer(char * buffer, int length) {
-  for (int i = 0; i < length; i++) {
-      buffer[i] = NULL;
-  }
-}
-
-String  try_to_read_card() {
-  static char buffer[64];       // Een buffer voor de gelezen data
+int  read_rfid_card() {
   static int counter = 0;       // Een teller voor buffer
-  String id = "";               // Het uiteindelijke ID
+  int sizeOfId = 0;
 
-  while(Serial.available()) {
-    char newChar = Serial.read();   // Lees karakter van Serial
-
-    // ASCII 02: STX (Start of Text)
-    // ASCII 03: ETX (End of Text)
-    if (newChar != 2 && newChar != 3 && counter < 64) {
-      buffer[counter++] = newChar;
-    } else {
-      if (newChar == 3) {
-        id = String(buffer);
+  while (sizeOfId != 12) {
+    while(Serial.available()) {
+      char newChar = Serial.read();   // Lees karakter van Serial
+      // ASCII 02: STX (Start of Text)
+      // ASCII 03: ETX (End of Text)
+      if (newChar != 2 && newChar != 3 && counter < SIZE_OF_BUFFER) {
+        buffer[counter++] = newChar;
+        buffer[counter] = '\0';
+        sizeOfId = counter;
+      } else {
+        counter = 0;
       }
-      clear_buffer(buffer, 64);
-      counter = 0;
     }
   }
  
-  return id;
+  return sizeOfId;
 }
