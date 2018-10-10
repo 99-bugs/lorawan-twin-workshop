8579fe00cadec5b08150078b3ed4373cb3d84b2f
Nico De Witte <nico.dewitte@vives.be>
2018-10-10 10:33:52 +0200

    Refactor push button to new event based system

--- examples/sensors/push_button_event_driven/push_button_event_driven.ino
+++ examples/sensors/push_button_event_driven/push_button_event_driven.ino
@@ -1,37 +1,30 @@
 const int pushPin = 15;   // Pin van de drukknop
 
-// Vorige staat van de drukknop (niet ingedrukt)
-int previousState = LOW;
-
 void setup()
 {
   // put your setup code here, to run once:
   SerialUSB.begin(115200);
-  while ((!SerialUSB) && (millis() < 30000));
+  while ((!SerialUSB) && (millis() < 5000));
   SerialUSB.println("Starten van push button demo");
   pinMode(pushPin, INPUT);          // Digitale pin als ingang
-  pinMode(LED_BLUE, OUTPUT);        // Blauwe LED als uitgang
-  digitalWrite(LED_BLUE, HIGH);     // LED uit (geinverteerd)
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
+  // Lees de huidige stand van de drukknop
+  int previousState = digitalRead(pushPin);
+  int state = previousState;
 
-  // Lees de huidige stand van de drukknop in
-  int pushState = digitalRead(pushPin);
+  SerialUSB.println("Wachten voor event");
 
-  if (pushState != previousState) {
-    if (pushState == HIGH) {
-      // LED aan
-      digitalWrite(LED_BLUE, LOW);    // Geinverteerd
-      SerialUSB.println("De drukknop is ingedrukt");
-    }
-    else {
-      // LED uit
-      digitalWrite(LED_BLUE, HIGH);    // Geinverteerd
-      SerialUSB.println("De drukknop is niet ingedrukt");
-    }
-    previousState = pushState;
+  // Wachten op verandering van de staat van de knop.
+  // We wachten ook zolang de knop ingedrukt is (state == LOW)
+  //    (loslaten negeren we dus, enkel indrukken)
+  while (state == previousState || state  == LOW) {
+    previousState = state;          // Nieuwe staat opslaan in oude staat
+    state = digitalRead(pushPin);    // Nieuwe staat inlezen
+    delay(10);    // Even wachten voor ontdendering
   }
+
+  SerialUSB.println("Event is gebeurt");
 }
--- examples/sensors/push_button_start_sketch/push_button_start_sketch.ino
+++ examples/sensors/push_button_start_sketch/push_button_start_sketch.ino
@@ -4,28 +4,20 @@ void setup()
 {
   // put your setup code here, to run once:
   SerialUSB.begin(115200);
-  while ((!SerialUSB) && (millis() < 30000));
+  while ((!SerialUSB) && (millis() < 5000));
   SerialUSB.println("Starten van push button demo");
   pinMode(pushPin, INPUT);          // Digitale pin als ingang
-  pinMode(LED_BLUE, OUTPUT);        // Blauwe LED als uitgang
-  digitalWrite(LED_BLUE, HIGH);     // LED uit (geinverteerd)
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
-
   // Lees de huidige stand van de drukknop in
   int pushState = digitalRead(pushPin);
 
   if (pushState == HIGH) {
-    // LED aan
-    digitalWrite(LED_BLUE, LOW);    // Geinverteerd
     SerialUSB.println("De drukknop is ingedrukt");
   }
   else {
-    // LED uit
-    digitalWrite(LED_BLUE, HIGH);    // Geinverteerd
     SerialUSB.println("De drukknop is niet ingedrukt");
   }
 
