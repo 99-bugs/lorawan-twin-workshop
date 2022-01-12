211541be848d30604013b7129cb0ddd9928443a4
Nico De Witte <nico.dewitte@vives.be>
2022-01-12 14:06:49 +0100

    Refactor nodered chapter

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -35,31 +35,19 @@ module.exports = {
       {
         text: 'Het Internet',
         children: [
-          {
-            text: 'LoRaWAN',
-            link: '/lorawan/',
-          },
-          {
-            text: 'MQTT',
-            link: '/mqtt/',
-          }
+          '/lorawan/',
+          '/mqtt/'
+        ]
+      },
+      {
+        text: 'De applicatie',   // required
+        children: [
+          '/node-red/'
         ]
       },
       // Visualuatie ?
       // Interactie ?
       // NodeRED + NodeRED dashboard + eventueel eigen dashboard ?
-
-
-      // {
-      //   title: 'De server',   // required
-      //   collapsable: true, // optional, defaults to true
-      //   sidebarDepth: 0,    // optional, defaults to 1
-      //   children: [
-      //     '/lamp/',
-      //     '/mosquitto/',
-      //     '/node-red/'
-      //   ]
-      // },
       // {
       //   title: 'Het dashboard',   // required
       //   path: '/iot-dashboard/',      // optional, which should be a absolute path.
